a861330bcd24f643a6930544a1a76c84af96cba2
Michael Bocon <mikebocon@gmail.com>
2022-07-12 17:02:50 -1000

    wip: adds basic config

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Dependencies
+const express = require('express');
 
 // Initialize the Express App
+const app = express();
 
 // Configure application settings
 
 // Mount middleware
 
 // Mount routes
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+})
 
 // Tell the app to listen for requests from the client
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
