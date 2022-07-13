208831f953b7c01aa7ebdd818f873778fb8b8979
Michael Bocon <mikebocon@gmail.com>
2022-07-12 16:49:43 -1000

    wip: makes initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// Dependencies
+
+// Initialize the Express App
+
+// Configure application settings
+
+// Mount middleware
+
+// Mount routes
+
+// Tell the app to listen for requests from the client
